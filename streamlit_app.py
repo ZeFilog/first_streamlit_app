6b3a7c55497e47dcf088f508c59f2fab7ccfbb41
ZeFilog <josselin.honore@protonmail.com>
2023-03-27 13:59:12 +0200

    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('Page Test Josselin Honoré')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
